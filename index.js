0df863caafa83fd4cac038bdd56de800d9dde42b
Marcelina <marcelina.romm@gmail.com>
2023-04-13 19:40:55 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const tablica = [
+    {
+    imageUrl: 'https://..../xxx.png',
+    headline: 'Najlepszy produkt 1',
+    description: 'Opis produktu 1',
+    identifier: 'id1',
+    },
+    {
+    imageUrl: 'https://..../yyy.png',
+    headline: 'Najlepszy produkt 2',
+    description: 'Opis produktu 2',
+    identifier: 'id2',
+    },
+    {
+    imageUrl: 'https://..../yyy.png',
+    headline: 'Najlepszy produkt 2',
+    description: 'Opis produktu 2',
+    identifier: 'id3',
+    },
+    {
+    imageUrl: 'https://..../yyy.png',
+    headline: 'Najlepszy produkt 2',
+    description: 'Opis produktu 2',
+    identifier: 'id4',
+    },
+    ]
